e5496ec940f02fbd1ea71283a07ecb77ee12a9d7
AAashik519 <alamin35-519@diu.edu.bd>
2024-04-29 00:29:04 +0600

    Login page and register pages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,36 +1,42 @@
-import {
-    createBrowserRouter,
- 
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      // {
+      //     path:'/order',
+      //     element:<Order />
+      // },
+      {
+        path: "/order/:category",
+        element: <Order />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
-  const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Main />,
-        children :[
-            {
-                path:'/',
-                element:<Home />
-            },
-            {
-                path:'/menu',
-                element: <Menu />
-            },
-            // {
-            //     path:'/order',
-            //     element:<Order />
-            // },
-            {
-                path:'/order/:category',
-                element:<Order />
-            },
-        ]
-    }
-  ])
-
-  export default router
+export default router;
